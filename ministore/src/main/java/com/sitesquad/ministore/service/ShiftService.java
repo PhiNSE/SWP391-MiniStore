9b7134842941df6a4087feab2a07862fbd5ed71b
PhiNSE <nguyenphigv23@gmail.com>
2023-05-24 19:33:30 +0700

    phi

---
+++ ministore/src/main/java/com/sitesquad/ministore/service/ShiftService.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sitesquad.ministore.service;
+
+import com.sitesquad.ministore.model.Shift;
+import com.sitesquad.ministore.repository.ShiftRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author ACER
+ */
+@Service
+public class ShiftService {
+    @Autowired
+    ShiftRepository shiftRepository;
+    
+    public List<Shift> findAll(){
+        return shiftRepository.findAll();
+    }
+    
+    public Shift findById(Long id){
+        return shiftRepository.findById(id).get();
+    }
+    
+    public Shift add(Shift shift){
+        return shiftRepository.save(shift);
+    }
+    
+    public boolean delete(Long id){
+        shiftRepository.deleteById(id);
+        return shiftRepository.findById(id) == null;
+    }
+}
