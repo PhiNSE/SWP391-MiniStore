7f9b6c7692c01fb52461851126fdd7ac9bbe3361
Duc bao <baondforwork@gmail.com>
2023-05-31 08:52:35 +0700

    adding config for FE

--- ministore/src/main/java/com/sitesquad/ministore/controller/OrderCreator.java
+++ ministore/src/main/java/com/sitesquad/ministore/controller/OrderCreator.java
@@ -26,11 +26,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.CrossOrigin;
 /**
  *
  * @author admin
  */
+@CrossOrigin
 @RestController
 public class OrderCreator {
 
