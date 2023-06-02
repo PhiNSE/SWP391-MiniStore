46a790958db3301bd5c43fb542ede7e4fe3a1185
baotam546 <donguyenbaotam546@gmail.com>
2023-06-02 12:21:41 +0700

    test

--- ministore/src/main/java/com/sitesquad/ministore/config/CustomWebConfig.java
+++ ministore/src/main/java/com/sitesquad/ministore/config/CustomWebConfig.java
@@ -4,7 +4,6 @@ import com.sitesquad.ministore.model.RequestMeta;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Scope;
 import org.springframework.web.context.annotation.RequestScope;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
