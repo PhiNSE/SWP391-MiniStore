0ef13b085701662550c7aca5a62a63a077605a93
Duc bao <baondforwork@gmail.com>
2023-06-04 16:32:45 +0700

    adding cross

--- ministore/src/main/java/com/sitesquad/ministore/controller/admin/VoucherController.java
+++ ministore/src/main/java/com/sitesquad/ministore/controller/admin/VoucherController.java
@@ -10,6 +10,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,6 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
  *
  * @author admin
  */
+@CrossOrigin
 @RestController
 public class VoucherController {
 
