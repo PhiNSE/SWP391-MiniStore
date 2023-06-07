916155cd757fa360b164f2eb46d2a39584e6e9a5
baotam546 <donguyenbaotam546@gmail.com>
2023-06-07 07:52:53 +0700

    test database push

--- Test-db-insert.sql
+++ Test-db-insert.sql
@@ -6,7 +6,7 @@ insert into [dbo].[role] values('employee',20.00,0)
 GO
 insert into [dbo].[role] values('guard',15.00,0)
 GO
-insert into [dbo].[tbl_user]([name],[email],[password],[phone],[address],[role_id],[is_deleted]) values('admintest','test@gmail.com','1','0931311835','111 test street','1','false')
+insert into [dbo].[tbl_user]([name],[email],[password],[phone],[address],[role_id],[is_deleted],[dob],[gender],[user_img]) values('admintest','test@gmail.com','1','0931311835','111 test street','1','false','2003-05-14',1,'tesst')
 GO
 insert into [dbo].[shift]([name],[start_work_hour],[end_work_hour],[coefficient]) values('saler-morning',6,12,1),
 ('saler-afternoon',12,18,1),
