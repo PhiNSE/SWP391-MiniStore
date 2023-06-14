9cef8a818cb3b3510982ae6f6841a4fbc47cf0d2
baotam546 <donguyenbaotam546@gmail.com>
2023-06-14 21:04:00 +0700

    change

--- ministore/src/main/java/com/sitesquad/ministore/controller/shift/UserShiftController.java
+++ ministore/src/main/java/com/sitesquad/ministore/controller/shift/UserShiftController.java
@@ -20,10 +20,7 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  *
@@ -67,7 +64,7 @@ public class UserShiftController {
         );
     }
 
-    @GetMapping("/userShift/assign")
+    @PostMapping("/userShift/assign")
     public ResponseEntity<ResponseObject> assignUserShift(@RequestBody(required = false) List<Map<String, Object>> request) {
         List<UserShift> assignedUserShifts = new ArrayList<>();
         if (request == null || request.isEmpty()) {
