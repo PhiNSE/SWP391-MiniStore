c71e97ae592c3f55a6a52a63d052a37c6bac1e37
PhiNSE <nguyenphigv23@gmail.com>
2023-06-23 19:49:18 +0700

    auto check late 2

--- ministore/src/main/java/com/sitesquad/ministore/service/shift/UserShiftService.java
+++ ministore/src/main/java/com/sitesquad/ministore/service/shift/UserShiftService.java
@@ -173,9 +173,9 @@ public class UserShiftService {
 
         return userShift;
     }
-
-//    @Scheduled(cron = "0 0 6,12,18 * * *")
-    @Scheduled(cron = "*/5 * * * * *")
+    
+//    @Scheduled(cron = "*/5 * * * * *")
+    @Scheduled(cron = "0 0 6,12,18 * * *")
     public List<UserShift> checkInLateChecker() {
         //test
 //        List<UserShift> test = userShiftRepository.findUserShiftsByStartTime(
