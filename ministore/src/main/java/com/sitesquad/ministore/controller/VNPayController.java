b72d57b417fb0851eb492bca3dba9093fbc914ad
PhiNSE <nguyenphigv23@gmail.com>
2023-07-20 22:21:27 +0700

    vn-pay

--- ministore/src/main/java/com/sitesquad/ministore/controller/VNPayController.java
+++ ministore/src/main/java/com/sitesquad/ministore/controller/VNPayController.java
@@ -1,9 +1,12 @@
 package com.sitesquad.ministore.controller;
 
 
+import com.sitesquad.ministore.dto.ResponseObject;
 import com.sitesquad.ministore.service.VNPayService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,18 +20,19 @@ public class VNPayController {
     private VNPayService vnPayService;
 
     @PostMapping("/submitOrder")
-    public String submidOrder(@RequestParam("amount") int orderTotal,
+    public ResponseEntity<ResponseObject> submitOrder(@RequestParam("amount") int orderTotal,
                               @RequestParam("orderInfo") String orderInfo,
                               HttpServletRequest request){
         String baseUrl = request.getScheme() + "://" + request.getServerName() + ":" + request.getServerPort();
         String vnpayUrl = vnPayService.createOrder(orderTotal, orderInfo, baseUrl);
         System.out.println(vnpayUrl);
-        return "redirect:" + vnpayUrl;
-
+//        return "redirect:" + vnpayUrl;
+     return ResponseEntity.status(HttpStatus.OK).body(
+                new ResponseObject(200, "Submit order to VN-PAY successfully", vnpayUrl));
     }
 
     @GetMapping("/vnpay-payment")
-    public String GetMapping(HttpServletRequest request, Model model){
+    public ResponseEntity<ResponseObject> GetMapping(HttpServletRequest request, Model model){
         int paymentStatus =vnPayService.orderReturn(request);
 
         String orderInfo = request.getParameter("vnp_OrderInfo");
@@ -40,7 +44,13 @@ public class VNPayController {
         model.addAttribute("totalPrice", totalPrice);
         model.addAttribute("paymentTime", paymentTime);
         model.addAttribute("transactionId", transactionId);
+        if(paymentStatus == 1) {
+            return ResponseEntity.status(HttpStatus.OK).body(
+                    new ResponseObject(200, "Order is paid by VN-PAY successfully", ""));
+        }else{
+            return ResponseEntity.status(HttpStatus.OK).body(
+                    new ResponseObject(500, "Order failed to be paid by VN-PAY", ""));
+        }
 
-        return paymentStatus == 1 ? "ordersuccess" : "orderfail";
     }
 }
