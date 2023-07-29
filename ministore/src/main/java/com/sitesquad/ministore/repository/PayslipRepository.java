7318ef1898c3a27cf1f7ff6ea8bf365de46a5a36
trmdat <tranminhdat0805@gmail.com>
2023-07-29 22:40:47 +0700

    dat

--- ministore/src/main/java/com/sitesquad/ministore/repository/PayslipRepository.java
+++ ministore/src/main/java/com/sitesquad/ministore/repository/PayslipRepository.java
@@ -17,16 +17,17 @@ import org.springframework.stereotype.Repository;
 public interface PayslipRepository extends JpaRepository<Payslip, Long>, JpaSpecificationExecutor<Payslip>{
     List<Payslip> findByUserId(Long userId);
     List<Payslip> findByIsPaidNullOrIsPaidFalse();
-    @Query(value = "SELECT \n" +
-            "    tbl_user.user_id,\n" +
-            "    SUM(shift_count) AS 'sum_shift_count', \n" +
-            "    SUM(salary) AS 'sum_salary', \n" +
-            "    SUM(total_hours) AS 'sum_total_hours'\n" +
-            "FROM \n" +
-            "    tbl_user\n" +
-            "LEFT JOIN \n" +
-            "    payslip ON payslip.user_id = tbl_user.user_id AND (is_paid = 0 OR is_paid IS NULL)\n" +
-            "GROUP BY \n" +
-            "    tbl_user.user_id;", nativeQuery = true)
+    @Query(value = "SELECT\n" +
+            "                tbl_user.user_id,\n" +
+            "                SUM(shift_count) AS 'sum_shift_count',\n" +
+            "                SUM(salary) AS 'sum_salary',\n" +
+            "                SUM(total_hours) AS 'sum_total_hours'\n" +
+            "            FROM\n" +
+            "                tbl_user\n" +
+            "            LEFT JOIN \n" +
+            "                payslip ON payslip.user_id = tbl_user.user_id AND (is_paid = 0 OR is_paid IS NULL)\n" +
+            "            WHERE tbl_user.is_deleted is null or tbl_user.is_deleted = 0\n" +
+            "            GROUP BY \n" +
+            "                tbl_user.user_id;", nativeQuery = true)
     List<Map<String, Object>> findByCustom();
 }
